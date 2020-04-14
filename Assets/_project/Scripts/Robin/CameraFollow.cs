4359617543a214ff5938814c41fa919b1ff2e9de
Leonis26 <leonis2616@gmail.com>
2020-04-14 10:39:50 +0200

    Fixed camera follow with offset
    - Fixed camera (on Y)

--- Assets/_project/Scripts/Robin/CameraFollow.cs
+++ Assets/_project/Scripts/Robin/CameraFollow.cs
@@ -7,12 +7,18 @@ public class CameraFollow : MonoBehaviour
     // Start is called before the first frame update
     public float Speed;
     public GameObject Target;
+    private float screenWidth;
+
+    private void Start()
+    {
+        screenWidth = Camera.main.orthographicSize;
+    }
 
     // Update is called once per frame
     void LateUpdate()
     {
         float distance = Vector2.Distance(Camera.main.transform.position, Target.transform.position);
-
-        Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, Target.transform.position+Vector3.forward*-10, Speed * Time.deltaTime * distance);
+        var currentTarget = new Vector3(Target.transform.position.x + screenWidth, -10, -10);
+        Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, currentTarget + Vector3.forward*-10, Speed * Time.deltaTime * distance);
     }
 }
