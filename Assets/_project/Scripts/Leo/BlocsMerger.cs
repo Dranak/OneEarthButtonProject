74799bdfb0ee49c5266ccaa46235e73583681626
Leonis26 <leonis2616@gmail.com>
2020-05-27 12:37:40 +0200

    Blocs Merger Editor
    (no com)

---
+++ Assets/_project/Scripts/Leo/BlocsMerger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BlocsMerger : MonoBehaviour
+{
+    public List<BlocsStorageScriptableObject> blocsToMerge;
+    public string mergedBlocPath = "Assets/_project/CreatorTool/LD/";
+    [HideInInspector] [SerializeField] public BlocsStorageScriptableObject mergedBloc;
+}
