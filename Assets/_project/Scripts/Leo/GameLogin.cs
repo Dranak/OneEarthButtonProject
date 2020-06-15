9e1a40dbdb7418d39a6cf7cfde66d72995f20849
Leonis26 <leonis2616@gmail.com>
2020-06-15 11:45:34 +0200

    New DeathInfo Analytics struct

--- Assets/_project/Scripts/Leo/GameLogin.cs
+++ Assets/_project/Scripts/Leo/GameLogin.cs
@@ -7,22 +7,43 @@ public class GameLogin : MonoBehaviour
     public void OnGameOver(Player _player, Obstacle _obstacle)
     {
         var _obsParams = (ObstacleSpawnable)_obstacle.GetSpawnable();
-        var _obsSize = _obstacle.Size;
-        if (_obsSize.x != _obsSize.y)
-            _obsSize = _obsParams.BoundsSize;
-        
+
+        var playerPos = (Vector2)_player.WormHead.transform.position;
+        var playerSpeed = _player.WormHead.Rigidbody.velocity;
+        var obstaclePos = (Vector2)_obstacle.transform.position;
+
         Analytics.CustomEvent("gameOver", new Dictionary<string, object>
         {
             // player info
             { "deathLevel", _player.playingBlocName },
             { "ecoPoints", _player.Score },
-            { "playerPosition",  (Vector2)_player.WormHead.transform.position },
-            { "speedVector", _player.WormHead.Rigidbody.velocity }, // won't work
+            { "playerPosition",  playerPos},
+            { "speedVector", playerSpeed}, // won't work
 
             // obstacle info
             { "obstacleName", _obsParams.Tag},
-            { "obstaclePosition", (Vector2)_obstacle.transform.position },
-            { "obstacleBoundsSize", _obsSize }
+
+            // general info stacked
+            { "Death Info", new DeathInfo(playerPos, obstaclePos, _obstacle.Size, playerSpeed, _player.playingBlocName, _obsParams.Tag, _obsParams.BodyRotation, _player.Score) }
         });
     }
+}
+
+struct DeathInfo
+{
+    public DeathInfo(Vector2 plyPos, Vector2 obsPos, Vector2 obsSize, Vector2 speed, string bloc, string obsName, float obsRot, int _score)
+    {
+        playerPosition = plyPos;
+        obstaclePos = obsPos;
+        obstacleSize = obsSize;
+        speedVector = speed;
+        blocName = bloc;
+        obstacleName = obsName;
+        obstacleRotation = obsRot;
+        score = _score;
+    }
+    Vector2 playerPosition, obstaclePos, obstacleSize, speedVector;
+    string blocName, obstacleName;
+    float obstacleRotation;
+    int score;
 }
