453a62124edb2ee01c64a50f4afb59f202d20584
Dranak <robin.lacomblez@gmail.com>
2020-06-25 10:22:49 +0200

    LightSetting

---
+++ Assets/_project/Scripts/Robin/BlinkingLight.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class BlinkingLight : MonoBehaviour
+{
+    public float DurationBlink;
+    public Light2D Light;
+    public float MinIntensity;
+    public float MaxIntensity;
+    private float _chronoBlink;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Light.intensity = MinIntensity;
+        Blink();
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Blink()
+    {
+        _chronoBlink += Time.deltaTime;
+        if (_chronoBlink >= DurationBlink)
+        {
+            _chronoBlink = 0f;
+        }
+        float scaledTime = _chronoBlink / DurationBlink;
+        Light.intensity = Mathf.Lerp(MinIntensity, MaxIntensity, Mathf.PingPong(scaledTime,1f));
+    }
+
+}
--- Assets/_project/Scripts/Robin/SkyManager.cs
+++ Assets/_project/Scripts/Robin/SkyManager.cs
@@ -6,28 +6,45 @@ using UnityEngine.Experimental.Rendering.Universal;
 public class SkyManager : MonoBehaviour
 {
     public GameObject CenterUniverse;
+    public Light2D Sun;
+    public Light2D Moon;
+
     public MeshRenderer Sky;
     public float DurationOfCycle;
     private float _chronoCycle = 0;
+    private float _scaleTime = 0f;
+    private float _timeOfDay = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         _chronoCycle += Time.deltaTime;
-        float scaleTime = _chronoCycle / DurationOfCycle;
+        _scaleTime = _chronoCycle / DurationOfCycle;
 
         if (_chronoCycle == DurationOfCycle)
         {
             _chronoCycle = 0f;
         }
-        float timeOfDay = Mathf.Lerp(0f, 1f, Mathf.PingPong(scaleTime, 1f));
+        _timeOfDay = Mathf.Lerp(0f, 1f, Mathf.PingPong(_scaleTime, 1f));
         //Debug.Log("TimeOfDay " + timeOfDay);
-        Sky.material.SetFloat("_TimeOfDay", timeOfDay);
-        
+        Sky.material.SetFloat("_TimeOfDay", _timeOfDay);
+        RotateStar();
+
     }
+
+    void RotateStar()
+    {
+        Sun.transform.RotateAround(CenterUniverse.transform.position,Vector3.forward, Mathf.LerpAngle(0, 360, _timeOfDay));
+        Moon.transform.RotateAround(CenterUniverse.transform.position,Vector3.forward, Mathf.LerpAngle(0, 360, _timeOfDay));
+
+    }
+
+
+
+
 }
