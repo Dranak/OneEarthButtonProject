cbca142116f923f295683677bbd01a31e43c85cd
Leonis26 <leonis2616@gmail.com>
2020-06-30 17:06:10 +0200

    Dressing unfolder list

---
+++ Assets/_project/Scripts/Leo/UnfoldDragger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UnfoldDragger : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IPointerClickHandler
+{
+    [SerializeField] UnfoldButton unfoldManager;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        SelectUnfolder(unfoldManager.selectedUnfolder);
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        SelectUnfolder(unfoldManager.selectedUnfolder);
+    }
+
+    void SelectUnfolder(Unfolder unfolder)
+    {
+        if (unfolder)
+            UiManager.Instance.eventSystem.SetSelectedGameObject(unfolder.gameObject);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (unfoldManager.selectedUnfolder)
+        {
+            unfoldManager.Folding(unfoldManager.selectedUnfolder);
+        }
+    }
+}
